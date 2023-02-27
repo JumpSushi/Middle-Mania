84607ae5adf181f22f967b2d5e0233792262e8ac
JumpSushi <99227581+JumpSushi@users.noreply.github.com>
2023-02-27 14:29:28 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,3 @@
-function Set_level () {
-    while (true) {
-        if (input.buttonIsPressed(Button.A)) {
-        	
-        } else if (input.buttonIsPressed(Button.B)) {
-            Level += 1
-            basic.showString("" + Level)
-        } else if (Level <= 0) {
-            Level = 0
-        } else if (input.buttonIsPressed(Button.AB)) {
-            Position = game.createSprite(2, 2)
-            break;
-        }
-    }
-}
 input.onButtonPressed(Button.A, function () {
     if (Position.get(LedSpriteProperty.X) == 2) {
         game.addScore(1)
@@ -22,14 +7,11 @@ input.onButtonPressed(Button.A, function () {
     }
 })
 let Intervine = 0
-let Level = 0
 let Position: game.LedSprite = null
 Position = game.createSprite(2, 2)
-Level = 1
-Intervine = 500
+let Level = 1
+Intervine = randint(400, 500)
 game.setScore(0)
-Position.delete()
-Set_level()
 basic.forever(function () {
     Position.move(1)
     Position.ifOnEdgeBounce()
