2189ef0585e78ea17df0e344060664ed1d12cc2b
JumpSushi <99227581+JumpSushi@users.noreply.github.com>
2023-02-27 14:56:30 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,8 +2,10 @@ input.onButtonPressed(Button.A, function () {
     if (Position.get(LedSpriteProperty.X) == 2) {
         game.addScore(1)
         Intervine = Intervine - 50
+        music.playMelody("C D E F G A B C5 ", 500)
     } else {
         game.gameOver()
+        music.playMelody("C D E F G A B C5 ", 500)
     }
 })
 let Intervine = 0
@@ -18,14 +20,12 @@ basic.forever(function () {
         Position.move(randint(-2, 2))
         music.playTone(262, music.beat(BeatFraction.Whole))
         Position.change(LedSpriteProperty.Y, randint(-2, 2))
-        music.playTone(262, music.beat(BeatFraction.Whole))
         Position.ifOnEdgeBounce()
         basic.pause(Intervine)
     } else {
         Position.move(1)
         music.playTone(262, music.beat(BeatFraction.Whole))
         Position.change(LedSpriteProperty.Y, randint(-1, 1))
-        music.playTone(262, music.beat(BeatFraction.Whole))
         Position.ifOnEdgeBounce()
         basic.pause(Intervine)
     }
