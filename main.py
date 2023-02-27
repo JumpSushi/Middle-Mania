ab2411a0a19097c9d95d19a45a9ba0276a9e1546
JumpSushi <99227581+JumpSushi@users.noreply.github.com>
2023-02-27 14:14:44 +0800

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,28 +1,35 @@
-def Main():
+def Set_level():
+    global Level, Position
+    while True:
+        if input.button_is_pressed(Button.A):
+            pass
+        elif input.button_is_pressed(Button.B):
+            Level += 1
+            basic.show_string("" + str(Level))
+        elif Level <= 0:
+            Level = 0
+        elif input.button_is_pressed(Button.AB):
+            Position = game.create_sprite(2, 2)
+            break
+
+def on_button_pressed_a():
     global Intervine
     if Position.get(LedSpriteProperty.X) == 2:
         game.add_score(1)
         Intervine = Intervine - 50
     else:
         game.game_over()
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
 Intervine = 0
-Position: game.LedSprite = None
 Level = 0
-basic.show_string("s")
-while True:
-    if input.button_is_pressed(Button.A):
-        Level += 1
-        basic.show_string("" + str((Level)))
-    elif input.button_is_pressed(Button.B):
-        Level += -1
-        basic.show_string("" + str((Level)))
-    elif Level <= 0:
-        Level = 1
-    elif input.button_is_pressed(Button.AB):
-        Main()
-        break
+Position: game.LedSprite = None
 Position = game.create_sprite(2, 2)
-Intervine = 600
+Level = 1
+Intervine = 500
+game.set_score(0)
+Position.delete()
+Set_level()
 
 def on_forever():
     Position.move(1)
--- main.ts
+++ main.ts
@@ -1,31 +1,35 @@
-function Main () {
+function Set_level () {
+    while (true) {
+        if (input.buttonIsPressed(Button.A)) {
+        	
+        } else if (input.buttonIsPressed(Button.B)) {
+            Level += 1
+            basic.showString("" + Level)
+        } else if (Level <= 0) {
+            Level = 0
+        } else if (input.buttonIsPressed(Button.AB)) {
+            Position = game.createSprite(2, 2)
+            break;
+        }
+    }
+}
+input.onButtonPressed(Button.A, function () {
     if (Position.get(LedSpriteProperty.X) == 2) {
         game.addScore(1)
         Intervine = Intervine - 50
     } else {
         game.gameOver()
     }
-}
+})
 let Intervine = 0
-let Position: game.LedSprite = null
 let Level = 0
-basic.showString("s")
-while (true) {
-    if (input.buttonIsPressed(Button.A)) {
-        Level += 1
-        basic.showString("" + (Level))
-    } else if (input.buttonIsPressed(Button.B)) {
-        Level += -1
-        basic.showString("" + (Level))
-    } else if (Level <= 0) {
-        Level = 1
-    } else if (input.buttonIsPressed(Button.AB)) {
-        Main()
-        break;
-    }
-}
+let Position: game.LedSprite = null
 Position = game.createSprite(2, 2)
-Intervine = 600
+Level = 1
+Intervine = 500
+game.setScore(0)
+Position.delete()
+Set_level()
 basic.forever(function () {
     Position.move(1)
     Position.ifOnEdgeBounce()
